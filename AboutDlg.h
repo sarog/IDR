1ca2e0ea70e80c24ff1900b2644351d7eb5e840f
Saro G. <saro@saromedia.com>
2026-03-02 10:50:05 -0500

    About dialog fixes

--- AboutDlg.h
+++ AboutDlg.h
@@ -33,11 +33,14 @@ class TFAboutDlg_11011981 : public TForm {
     TLabel *Label5;
     TLabel *Label6;
     TImage *Image1;
+    TBitBtn *bDonate;
     TLabel *Label7;
     TLabel *Label8;
     TLabel *lblHint;
     void __fastcall FormCreate(TObject *Sender);
-
+	void __fastcall lEmailClick(TObject *Sender);
+	void __fastcall lWWWClick(TObject *Sender);
+    void __fastcall bDonateClick(TObject *Sender);
 private:
 public:
     virtual __fastcall TFAboutDlg_11011981(TComponent *AOwner);
