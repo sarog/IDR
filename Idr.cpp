a24408c3c486bdeffd3d888012936c3b3204809b
Saro G. <saro@saromedia.com>
2026-03-03 10:45:30 -0500

    Add additional exception catch

--- Idr.cpp
+++ Idr.cpp
@@ -31,24 +31,30 @@ int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int) {
         Application->HelpFile = "";
         Application->MainFormOnTaskBar = true;
         Application->CreateForm(__classid(TFMain_11011981), &FMain_11011981);
-		Application->CreateForm(__classid(TFExplorer_11011981), &FExplorer_11011981);
-		Application->CreateForm(__classid(TFTypeInfo_11011981), &FTypeInfo_11011981);
-		Application->CreateForm(__classid(TFStringInfo_11011981), &FStringInfo_11011981);
-		Application->CreateForm(__classid(TFInputDlg_11011981), &FInputDlg_11011981);
-		Application->CreateForm(__classid(TFindDlg_11011981), &FindDlg_11011981);
-		Application->CreateForm(__classid(TFEditFunctionDlg_11011981), &FEditFunctionDlg_11011981);
-		Application->CreateForm(__classid(TFEditFieldsDlg_11011981), &FEditFieldsDlg_11011981);
-		Application->CreateForm(__classid(TFAboutDlg_11011981), &FAboutDlg_11011981);
-		Application->CreateForm(__classid(TFKBViewer_11011981), &FKBViewer_11011981);
-		Application->CreateForm(__classid(TFLegend_11011981), &FLegend_11011981);
-		Application->CreateForm(__classid(TFHex2DoubleDlg_11011981), &FHex2DoubleDlg_11011981);
-		Application->CreateForm(__classid(TFPlugins), &FPlugins);
-		Application->CreateForm(__classid(TFActiveProcesses), &FActiveProcesses);
-		Application->CreateForm(__classid(TFIdcSplitSize), &FIdcSplitSize);
-		Application->CreateForm(__classid(TFProgressBar), &FProgressBar);
-		Application->Run();
+        Application->CreateForm(__classid(TFExplorer_11011981), &FExplorer_11011981);
+        Application->CreateForm(__classid(TFTypeInfo_11011981), &FTypeInfo_11011981);
+        Application->CreateForm(__classid(TFStringInfo_11011981), &FStringInfo_11011981);
+        Application->CreateForm(__classid(TFInputDlg_11011981), &FInputDlg_11011981);
+        Application->CreateForm(__classid(TFindDlg_11011981), &FindDlg_11011981);
+        Application->CreateForm(__classid(TFEditFunctionDlg_11011981), &FEditFunctionDlg_11011981);
+        Application->CreateForm(__classid(TFEditFieldsDlg_11011981), &FEditFieldsDlg_11011981);
+        Application->CreateForm(__classid(TFAboutDlg_11011981), &FAboutDlg_11011981);
+        Application->CreateForm(__classid(TFKBViewer_11011981), &FKBViewer_11011981);
+        Application->CreateForm(__classid(TFLegend_11011981), &FLegend_11011981);
+        Application->CreateForm(__classid(TFHex2DoubleDlg_11011981), &FHex2DoubleDlg_11011981);
+        Application->CreateForm(__classid(TFPlugins), &FPlugins);
+        Application->CreateForm(__classid(TFActiveProcesses), &FActiveProcesses);
+        Application->CreateForm(__classid(TFIdcSplitSize), &FIdcSplitSize);
+        Application->CreateForm(__classid(TFProgressBar), &FProgressBar);
+        Application->Run();
     } catch (Exception &exception) {
         Application->ShowException(&exception);
+    } catch (...) {
+        try {
+            throw Exception("");
+        } catch (Exception &exception) {
+            Application->ShowException(&exception);
+        }
     }
     return 0;
 }
